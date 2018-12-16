99541a8021168e766b8302c5cea24d863d45b9ee
Ruben <74729@protonmail.com>
2018-12-16 21:25:30 +0100

    fix: Removed a bug where the program crashed when the 'super secret code' the user entered did not exist

--- app/views.py
+++ app/views.py
@@ -19,26 +19,26 @@ def login_view(request):
     if request.method == 'POST':
         form = request.POST.dict()
         if 'super-secret-code' in form:
+            message = ''
+            code = SuperSecretCode.objects.filter(code=form['super-secret-code'])
             if form['password'] != form['password-check']:
-                return render(request, 'app/login.html', {'message': 'Passwords do not match.'})
-            if CustomUser.objects.filter(email=form['email']).exists():
+                message = 'Passwords do not match.'
+            elif CustomUser.objects.filter(email=form['email']).exists():
                 message = 'This e-mail address already has an account.'
-                return render(request, 'app/login.html', {'message': message})
-            if not SuperSecretCode.objects.filter(code=form['super-secret-code']).exists():
+            elif not code.exists():
                 message = 'This super secret code does not exist.'
-                return render(request, 'app/login.html', {'message': message})
-            code = SuperSecretCode.objects.filter(code=form['super-secret-code']).first()
-            if code.activated:
+            elif code.first().activated:
                 message = 'This super secret code has already been used.'
-                return render(request, 'app/login.html', {'message': message})
+            if message != '':
+                return render(request, 'app/login.html', {'message': message, 'register': True})
             new_user = CustomUser.objects.create_user(email=form['email'],\
                 first_name=form['first-name'],\
                 password=form['password'],\
                 username=uuid4())
             new_user.save()
-            code.activated = True
-            code.user = new_user
-            code.save()
+            code.first().activated = True
+            code.first().user = new_user
+            code.first().save()
             user = authenticate(request, email=form['email'], password=form['password'])
             if user is None:
                 message = "Something went wrong while creating your account. Please try again."
