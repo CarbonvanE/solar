63391ed74ce45bf665c42ef37c8befbdaf92eee2
Ruben <74729@protonmail.com>
2018-12-18 19:13:29 +0100

    feat: Added a modal so users chan change their settings (not yet working)

---
+++ app/static/app/logic/settings.js
@@ -0,0 +1,11 @@
+$('#change-settings-modal').on('show.bs.modal', function (event) {
+    const button = $(event.relatedTarget);
+    const key = button.data('key');
+    const value = button.data('value');
+
+    const modal = $(this)
+    modal.find('.modal-title').text('Change your ' + key);
+    modal.find('.modal-body input').attr("placeholder", value);
+    modal.find('.modal-body input').val("");
+    modal.find('.modal-change-button').text('Change ' + key);
+})
