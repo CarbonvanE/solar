a46f099757c3cd2b0c5c90d3c7198acf04fdfb50
Ruben <74729@protonmail.com>
2018-12-20 17:34:36 +0100

    feat: Added a cache to store the weather symbol for up to 5 minutes so that the sunrise/sunset and current energy generation don't have to get retrieved for each individual request

--- functions.py
+++ functions.py
@@ -1,6 +1,9 @@
+""" All functions necessary to create and return views """
 from datetime import datetime
 import json
 import requests
+
+from django.core.cache import cache
 
 
 def is_it_night(lat, lng):
@@ -9,10 +12,10 @@ def is_it_night(lat, lng):
     response = requests.get(url)
     data = json.loads(response.content)['results']
 
-    time = datetime.utcnow()
+    utc_time = datetime.utcnow()
     sunrise = datetime.strptime(data['sunrise'], '%Y-%m-%dT%H:%M:%S+00:00')
     sunset = datetime.strptime(data['sunset'], '%Y-%m-%dT%H:%M:%S+00:00')
-    if  sunrise < time < sunset:
+    if  sunrise < utc_time < sunset:
         return False
     return True
 
@@ -28,13 +31,20 @@ def get_current_power(site_id, api_key):
 
 def what_is_the_weather(site_id, api_key, lat, lng):
     """ checks what the weather is and returns a FontAwesome class """
-    if is_it_night(lat, lng):
-        return 'fa-moon'
+    weather_symbol = cache.get('current_weather')
+    if weather_symbol is not None:
+        return weather_symbol
+    is_night = is_it_night(lat, lng)
     current_power = get_current_power(site_id, api_key)
-    if current_power > 1000:
-        return 'fa-sun'
-    if current_power > 100:
-        return 'fa-cloud-sun'
-    if current_power >= 0:
-        return 'fa-cloud'
-    return 'fa-exclamation-triangle'
+    if is_night:
+        weather_symbol = 'fa-moon'
+    elif current_power > 1000:
+        weather_symbol = 'fa-sun'
+    elif current_power > 100:
+        weather_symbol = 'fa-cloud-sun'
+    elif current_power >= 0:
+        weather_symbol = 'fa-cloud'
+    else:
+        weather_symbol = 'fa-exclamation-triangle'
+    cache.set('weather_symbol', weather_symbol, 5 * 60)
+    return weather_symbol
