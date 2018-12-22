43ae11d777e1916c7797979e64acd229711ab646
Ruben <74729@protonmail.com>
2018-12-22 17:01:44 +0100

    style: Improved the styling of import_data.py

--- import_data.py
+++ import_data.py
@@ -1,4 +1,6 @@
-import requests, json
+""" Imports all the initial data from the API to the database """
+import json
+import requests
 
 with open('solar/secrets.json', 'r') as f:
     API_KEY = json.load(f)['SOLAREDGE']['API_KEY']
@@ -14,9 +16,7 @@ def get_average(values, index_range):
         for i in range(-index_range, 0):
             total += values[i]['p_q']
         avg = round(total / index_range, 3)
-        return(avg)
-                # print(values[i])
-        # print(int(total / index_range), ';' , int(float(values[-3]['p_q'])) if values[-3]['p_q'] != None else 0)
+        return avg
 
 
 def get_power_quarterly():
@@ -31,8 +31,9 @@ def get_power_quarterly():
             date, time = datum['date'].split(' ')
             y, m, d = date.split('-')
             h, min, s = time.split(':')
-            avg_power = (0.000 if datum['value'] == None else round(datum['value'], 3))
-            all_values.append({'y': y, 'm':m, 'd':d, 'h': h, 'min': min, 's': s, 'p_q': avg_power, })
+            avg_power = (0.000 if datum['value'] is None else round(datum['value'], 3))
+            data = {'y': y, 'm':m, 'd':d, 'h': h, 'min': min, 's': s, 'p_q': avg_power}
+            all_values.append(data)
             # get_average(all_values, 5)
             if len(all_values) >= 5:
                 all_values[-3]['p_1'] = get_average(all_values, 5)
@@ -47,7 +48,7 @@ def get_power_quarterly():
         for i in all_values:
             # print(i)
             try:
-                print(i['p_1'], ';', i['p_3'], ';', i['p_24'], ';' ,i['p_7'])
+                print(i['p_1'], ';', i['p_3'], ';', i['p_24'], ';', i['p_7'])
             except:
                 print('')
     else:
